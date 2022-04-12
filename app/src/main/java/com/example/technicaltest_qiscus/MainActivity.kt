5e12657897cc9de2283bcdbe6aa7e5e7e2f667bd
muhammadaqilp <muhammadpratama1816@gmail.com>
2022-04-12 17:39:35 +0700

    initialize project

---
+++ app/src/main/java/com/example/technicaltest_qiscus/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.technicaltest_qiscus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
